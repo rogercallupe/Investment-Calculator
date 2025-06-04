dc7a92f2c606ce49897362dc547f3dd08564d13f
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-06-04 14:36:45 -0500

    Angular-Investment-Passing Data from Parent to Child with Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
   onCalculateInvestmentResults(data:InvestmentInput) {
   const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
   const annualData = [];
@@ -33,6 +43,6 @@ export class AppComponent {
     });
   }
 
-  console.log(annualData);
+this.resultsData = annualData;
 }
 }
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+// two ways to define the InvestmentInput type are provided below.
+ // result = input <{
+    //   year: year,
+    //   interest: interestEarnedInYear,
+    //   valueEndOfYear: investmentValue,
+    //   annualInvestment: annualInvestment,
+    //   totalInterest: totalInterest,
+    //   totalAmountInvested: initialInvestment + annualInvestment * year,
+    // }[]>(); 
+ // This component displays the results of the investment calculation.
+
+ @Input() results?: {
+      year: number,
+      interest: number,
+      valueEndOfYear: number,
+      annualInvestment: number,
+      totalInterest: number,
+      totalAmountInvested: number,
+    }[];// This is an array of objects representing the investment results for each year.
+
+}
